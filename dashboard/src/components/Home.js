eebd24924240d7f10c153e15359594557b71ab80
Ravi Kumar <ravikumarssp121@gmail.com>
2026-06-17 22:01:40 +0530

    dashboard frontend completed

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard';
+import Topbar from './TopBar';
+
+import '../index.css'
+function Home(){
+    return(
+        <div >
+            <Topbar/>
+            <Dashboard/>
+        </div>
+    )
+}
+export default Home;
