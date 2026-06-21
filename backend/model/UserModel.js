b3046b4761a40f9423d023eaa2a339682f56c60e
Mr. Ravi Kumar <131948467+Mrravikumar17@users.noreply.github.com>
2026-06-22 00:33:02 +0530

    backend setup and half done of authentication process (#2)
    marge backend pr

---
+++ backend/model/UserModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const {userSchema} = require("../schemas/UserSchema.js");
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+const userModel = new mongoose.model("user", userSchema);
+
+module.exports = {userModel};
